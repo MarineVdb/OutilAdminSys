03102b3d788e529d94b70f61a9ca76a5806755ad
Zelfrost <deconinck.damien@gmail.com>
2014-01-22 15:26:13 +0100

    Maniaqueries

--- supprimer.pl
+++ supprimer.pl
@@ -11,15 +11,15 @@ use Unicode::Normalize;
 ## Lecture de la ligne/du fichier ##
 ####################################
 
-if(@ARGV[0] eq "-n" || @ARGV[0] eq "--dry-run"){
+if(@ARGV[0] eq "-n" || @ARGV[0] eq "--dry-run") {
 	$description = 1;
 	shift;
 }
 
 while(<>) {
-        chomp;
-        #($nom, $prenom)= split(' ', $_);
-        supprimer($1, $2) if(/(.*)[,;:](.*)/);
+    chomp;
+    #($nom, $prenom)= split(' ', $_);
+    supprimer($1, $2) if(/(.*)[,;:](.*)/);
 }
 
 ######################################
@@ -31,40 +31,39 @@ sub supprimer {
 	$prenom = shift;
 	
 	if($nom eq "" || $prenom eq ""){
-		print "Il manque le nom ou prenom\n";
+		print "Il manque le nom ou prenom pour $nom $prenom\n";
 		next;
 	}	
 
 	$nom = caractereSpecial($nom);
 	$prenom = caractereSpecial($prenom);
 
-        $login = lc( substr($nom, 0, 7) . substr($prenom, 0, 1) );
+    $login = lc( substr($nom, 0, 7) . substr($prenom, 0, 1) );
 
-        $loginRetour = verification($login);
-        chomp($loginRetour);
-        $login = $loginRetour; 
+    $login = verification($login);
+    chomp($login);
 	
 	if($login eq "error") { next }
 
         $cheminLogin          = "/home/user/$login/"; 
         
-	if($description == 1){
+	if($description == 1) {
 		print "Suppression du groupe : $login\n";
 		print "Suppression de l'utilisateur : $login\n";
 		print "Suppression du répertoire : $cheminLogin\n\n";
-	}else{
+	} else {
 		#On supprime le login de user
-       		qx/ delgroup $login user /;
-        
-        	#On supprime l'utilisateur
-        	qx/ deluser $login /;
+   		qx/ delgroup $login user /;
+    
+    	#On supprime l'utilisateur
+    	qx/ deluser $login /;
 
-        	#On supprime l'intérieur du fichier
-        	`rm -rf $cheminLogin`;
+    	#On supprime l'intérieur du fichier
+    	`rm -rf $cheminLogin`;
 
-        	#On envoie en paramètre le login qui va être supprime du fichier log
-        	triListe($login);
-        	print "Le login $login n'existe plus, ainsi que son répertoire.\n";
+    	#On envoie en paramètre le login qui va être supprime du fichier log
+    	triListe($login);
+    	print "Le login $login n'existe plus, ainsi que son répertoire.\n";
 	}
 }
 
@@ -91,19 +90,19 @@ sub verification {
 
         return $loginRetour;
 
-    }elsif($nombreDeLigne == 1 && !($nomRetrouve eq $login)){
-	print "Le seul login retrouve avec les caractéristiques donnees est : ". $nomRetrouve;
-	print "Est-ce celui-ci que vous souhaitez supprimer ? (oui ou non) ";
-	
-	$reponse = <STDIN>;
-	chomp($reponse);
-	
-	if($reponse eq "oui"){
-		return $nomRetrouve;
-	}else{
-		return "error";	
-	}
-    }else{
+    } elsif($nombreDeLigne == 1 && $nomRetrouve ne $login) {
+    	print "Le seul login retrouve avec les caractéristiques donnees est : ". $nomRetrouve;
+    	print "Est-ce celui-ci que vous souhaitez supprimer ? (oui ou non) ";
+    	
+    	$reponse = <STDIN>;
+    	chomp($reponse);
+    	
+    	if($reponse eq "oui") {
+    		return $nomRetrouve;
+    	} else {
+    		return "error";	
+    	}
+    } else {
         return $login;
     }
 }
@@ -113,23 +112,23 @@ sub verification {
 ##########################################################
 
 sub triListe {
-        #Création du fichier log2 et ouverture du fichier log
-        open(LOG2, ">>log2") || die ("impossible d'ourir le fichier LOG. \n");
-        open(LOG, "log") || die ("Fichier LOG inexistant. \n");
-                while (my $ligne = <LOG>){
-                        #Si le début de la ligne ne correspond pas au login de la personne que l'on supprime
-                        #On l'ajoute au fichier lg2
-                        if ($ligne !~ /^$login;/){ 
-                               print LOG2 $ligne;
-                        }
-                } 
-        close(LOG); 
-        close(LOG2);
+    #Création du fichier log2 et ouverture du fichier log
+    open(LOG2, ">>log2") || die ("impossible d'ourir le fichier LOG. \n");
+    open(LOG, "log") || die ("Fichier LOG inexistant. \n");
+    while (my $ligne = <LOG>) {
+        #Si le début de la ligne ne correspond pas au login de la personne que l'on supprime
+        #On l'ajoute au fichier lg2
+        if ($ligne !~ /^$login;/) { 
+               print LOG2 $ligne;
+        }
+    } 
+    close(LOG); 
+    close(LOG2);
 
-        #On supprime l'ancien log pour en ouvrir un autre
-        `rm log`; 
-        #On modifie l'ancien fichier en le nouveau !
-        `mv log2 log`;
+    #On supprime l'ancien log pour en ouvrir un autre
+    `rm log`; 
+    #On modifie l'ancien fichier en le nouveau !
+    `mv log2 log`;
 }
 
 #####################################
--- supprimerLogin.pl
+++ supprimerLogin.pl
@@ -11,15 +11,14 @@ use Unicode::Normalize;
 ## Lecture de la ligne/du fichier ##
 ####################################
 
-if(@ARGV[0] eq "-n" || @ARGV[0] eq "--dry-run"){
+if(@ARGV[0] eq "-n" || @ARGV[0] eq "--dry-run") {
 	$description = 1;
 	shift;
 }
 
 while(<>) {
-        chomp;
-        #($nom, $prenom)= split(' ', $_);
-        supprimer($1) if(/(.*)/);
+    chomp;
+    supprimer($_);
 }
 
 ######################################
@@ -32,31 +31,31 @@ sub supprimer {
 	
 	$login = caractereSpecial($login);
 
-        $loginRetour = verification($login);
-        chomp($loginRetour);
-        $login = $loginRetour; 
+    $loginRetour = verification($login);
+    chomp($loginRetour);
+    $login = $loginRetour; 
 	
 	if($login eq "error") { print "le login n'existe pas\n"; next; }
 
-        $cheminLogin          = "/home/user/$login/";    
+    $cheminLogin = "/home/user/$login/";    
         
-	if($description == 1){
+	if($description == 1) {
 		print "Suppression de $login du groupe user \n";
 		print "Suppression de l'utilisateur : $login \n";
 		print "Suppression du répertoire : $cheminLogin\n\n";
-	}else{
+	} else {
 		#On supprime le login de user
-        	qx/ delgroup $login user /;
-        
-        	#On supprime l'utilisateur
-        	qx/ deluser $login /;
+    	qx/ delgroup $login user /;
+    
+    	#On supprime l'utilisateur
+    	qx/ deluser $login /;
 
-        	#On supprime l'intérieur du fichier
-        	`rm -rf $cheminLogin`;
+    	#On supprime l'intérieur du fichier
+    	`rm -rf $cheminLogin`;
 
-        	#On envoie en paramètre le login qui va être supprime du fichier log
-       		triListe($login);
-        	print "Le login $login n'existe plus, ainsi que son répertoire.\n";
+    	#On envoie en paramètre le login qui va être supprime du fichier log
+   		triListe($login);
+    	print "Le login $login n'existe plus, ainsi que son répertoire.\n";
 	}
 }
 
@@ -72,7 +71,7 @@ sub verification {
     
     if ($nombreDeLigne == 1) {
         return $login;
-    }else{
+    } else {
         return "error";
     }
 }
@@ -82,23 +81,23 @@ sub verification {
 ##########################################################
 
 sub triListe {
-        #Création du fichier log2 et ouverture du fichier log
-        open(LOG2, ">>log2") || die ("impossible d'ourir le fichier LOG. \n");
-        open(LOG, "log") || die ("Fichier LOG inexistant. \n");
-                while (my $ligne = <LOG>){
-                        #Si le début de la ligne ne correspond pas au login de la personne que l'on supprime
-                        #On l'ajoute au fichier lg2
-                        if ($ligne !~ /^$login;/){ 
-                               print LOG2 $ligne;
-                        }
-                } 
-        close(LOG); 
-        close(LOG2);
+    #Création du fichier log2 et ouverture du fichier log
+    open(LOG2, ">>log2") || die ("impossible d'ourir le fichier LOG. \n");
+    open(LOG, "log") || die ("Fichier LOG inexistant. \n");
+        while (my $ligne = <LOG>) {
+            #Si le début de la ligne ne correspond pas au login de la personne que l'on supprime
+            #On l'ajoute au fichier lg2
+            if ($ligne !~ /^$login;/) { 
+                   print LOG2 $ligne;
+            }
+        }
+    close(LOG); 
+    close(LOG2);
 
-        #On supprime l'ancien log pour en ouvrir un autre
-        `rm log`; 
-        #On modifie l'ancien fichier en le nouveau !
-        `mv log2 log`;
+    #On supprime l'ancien log pour en ouvrir un autre
+    `rm log`; 
+    #On modifie l'ancien fichier en le nouveau !
+    `mv log2 log`;
 }
 
 #####################################
